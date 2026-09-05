acd9ccfeca609cfafad56da7962f48d38007cbca
vanditv1008 <vijayvandit18@gmail.com>
2026-09-05 16:41:49 +0530

    financeagent

--- finance.py
+++ finance.py
@@ -3,17 +3,17 @@ from agno.models.google import Gemini
 from dotenv import load_dotenv
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.models.groq import Groq
-
+from agno.tools.yfinance import YFinanceTools
 
 def build_agent():
     load_dotenv()
 
     agent=Agent(
         model=Groq(id="openai/gpt-oss-20b"),
-        tools=[DuckDuckGoTools()],
-        instructions="You are a helpful travel assistant.",
+        tools=[ YFinanceTools(), DuckDuckGoTools() ],
+        description="This agent is designed to answer questions about finance and stock market data.",
         add_datetime_to_context=True,
-        
+        instructions="format your response with tables showing the data and provide a summary of the data in a paragraph at the end of the response."
         )
 
     return agent
@@ -22,5 +22,5 @@ def build_agent():
 groq_agent = build_agent()
 
 groq_agent.print_response(
-    "Is there a war in Iran?"
+    "share the NVDA stock price provide "
 )
